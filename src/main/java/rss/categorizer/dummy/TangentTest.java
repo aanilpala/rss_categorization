23ec748758de8eed35b885a67ad9edea46b51f64
Ahmet Anil Pala <aanilpala@gmail.com>
2015-02-11 01:40:00 +0100

    poster folder is deleted

--- src/main/java/rss/categorizer/dummy/TangentTest.java
+++
@@ -1,9 +0,0 @@
-package rss.categorizer.dummy;
-
-public class TangentTest {
-	public static void main(String[] args) {
-		
-		System.out.println(Math.atan(Math.tan(Math.PI/4))/(Math.PI/2));
-		
-	}
-}
